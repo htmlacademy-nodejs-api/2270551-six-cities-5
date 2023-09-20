0cee30419bd87a6ce9ac50f96ca1c849cc4de563
Greg <grtop@yandex.ru>
2023-09-20 22:16:27 +0300

    типизация офферов

--- src/shared/types/offer.type.ts
+++ src/shared/types/offer.type.ts
@@ -1,14 +1,25 @@
-import { OfferType } from './offer-type.enum.js';
-import { Category } from './category.type.js';
-import { User } from './user.type.js';
+import { Coords } from './coords.type.js';
+import { HouseType } from './house-type.enum.js';
+import { Feature } from './feature.enum.js';
+import { City } from './city.type.js';
+//import { User } from './user.type.js';
 
 export type Offer = {
-   title: string;
-   description: string;
-   postDate: Date;
-   image: string;
-   type: OfferType
-   price: number;
-   categories: Category[];
-   user: User;
- }
+  title: string;
+  description: string;
+  postDate: Date;
+  city: City;
+  preview: string;
+  photos: string[];
+  premium: boolean;
+  favorite: boolean;
+  rating: number;
+  houseType: HouseType;
+  roomNumber: number;
+  guests: number;
+  price: number;
+  features: Feature[];
+  author: string;
+  coments: number;
+  coords: Coords;
+};
