f71fe2e9a2847b28ff4c6347ccdf606657fd1953
Greg <grtop@yandex.ru>
2023-09-23 20:14:39 +0300

    Добавит тип MockServerData

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,4 +1,9 @@
-export * from './category.type.js';
-export * from './offer-type.enum.js';
+export * from './city.type.js';
+export * from './comment.type.js';
+export * from './coords.type.js';
+export * from './feature.enum.js';
+export * from './house-type.enum.js';
 export * from './user.type.js';
 export * from './offer.type.js';
+export * from './mock-server-data.type.js';
+
