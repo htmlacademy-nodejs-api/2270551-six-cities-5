a82d32e4d8122464e7ec6b191753e2a9409a8f05
Greg <grtop@yandex.ru>
2023-09-23 19:52:10 +0300

    откатим назад

--- src/shared/types/feature.enum.ts
+++ src/shared/types/feature.enum.ts
@@ -1,9 +1,11 @@
-export enum Feature {
-  breakfast = 'Breakfast',
-  conditioning = 'Air conditioning',
-  workspace = 'Laptop friendly workspace',
-  babySeat = 'Baby seat',
-  washer = 'Washer',
-  towels = 'Towels',
-  fridge = 'Fridge'
- }
+//export enum Feature {
+//  breakfast = 'Breakfast',
+//  conditioning = 'Air conditioning',
+//  workspace = 'Laptop friendly workspace',
+//  babySeat = 'Baby seat',
+//  washer = 'Washer',
+//  towels = 'Towels',
+//  fridge = 'Fridge'
+// }
+
+export type Feature = | 'Breakfast' | 'Air conditioning' | 'Laptop friendly workspace' | 'Baby seat' | 'Washer' | 'Towels' | 'Fridge'
