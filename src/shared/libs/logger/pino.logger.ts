f12881dc91b85cdbdd9512a67d590c06166aaba7
Greg <grtop@yandex.ru>
2023-10-07 19:39:13 +0300

    Добавит вывод сообщения в PinoLogger

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -29,6 +29,7 @@ export class PinoLogger implements LoggerInterface {
     });
 
     this.logger = pino({}, multiTransport);
+    this.logger.info('Logger created…');
   }
 
   public debug(message: string, ...args: unknown[]): void {
