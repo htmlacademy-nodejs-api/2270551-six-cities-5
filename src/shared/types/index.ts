3dc3c8fbebf3885eb4ae728f58a83d2419c59f59
Greg <grtop@yandex.ru>
2023-10-07 19:07:33 +0300

    Добавит перечисление Component

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -6,4 +6,6 @@ export * from './house-type.enum.js';
 export * from './user.type.js';
 export * from './offer.type.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';
+
 
