56079bc068f40ff00b1c4efba7ad93160c9f239e
Greg <grtop@yandex.ru>
2023-10-07 14:30:50 +0300

    модернизируем класс PinoLoger укжаем путь к логу

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,11 +1,22 @@
-import { Logger as PinoInstance, pino } from 'pino';
+import { Logger as PinoInstance, pino, transport } from 'pino';
 import { LoggerInterface } from './logger.interface.js';
+import { getCurrentModuleDirectoryPath } from '../../helpers/index.js';
+import { resolve } from 'node:path';
 
 export class PinoLogger implements LoggerInterface {
   private readonly logger: PinoInstance;
 
   constructor() {
-    this.logger = pino();
+    const modulePath = getCurrentModuleDirectoryPath();
+    const logFilePath = 'logs/rest.log';
+    const destination = resolve(modulePath, '../../../', logFilePath);
+
+    const fileTransport = transport({
+      target: 'pino/file',
+      options: { destination }
+    });
+
+    this.logger = pino({}, fileTransport);
   }
 
   public debug(message: string, ...args: unknown[]): void {
