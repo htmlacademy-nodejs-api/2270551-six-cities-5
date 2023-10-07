80126625ad69f1945f5b3605cad528872b65e095
Greg <grtop@yandex.ru>
2023-10-07 15:39:02 +0300

    добавим вывод значения переменной `PORT`

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,8 +1,10 @@
+import { Config } from '../shared/libs/config/config.interface.js';
 import { LoggerInterface } from '../shared/libs/logger/index.js';
 
 export class RestApplication {
   constructor(
-     private readonly logger: LoggerInterface
+     private readonly logger: LoggerInterface,
+     private readonly config: Config,
   ) {}
 
   public async init() {
@@ -10,5 +12,6 @@ export class RestApplication {
     this.logger.warn('Application initialization');
     this.logger.error('ehm', new Error('Some error'));
     this.logger.debug('This is debug');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
