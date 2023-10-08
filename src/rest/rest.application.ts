aaf4c309a92e2330f8c55d47c59b4fd85161cb5f
Greg <grtop@yandex.ru>
2023-10-08 16:15:18 +0300

    Добавит подключение к базе данных

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -2,13 +2,28 @@ import { inject, injectable } from 'inversify';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { LoggerInterface } from '../shared/libs/logger/index.js';
 import { Component } from '../shared/types/index.js';
+import { DatabaseClient } from '../shared/libs/database-client/index.js';
+import { getMongoURI } from '../shared/helpers/index.js';
 
 @injectable()
 export class RestApplication {
   constructor(
     @inject(Component.Logger) private readonly logger: LoggerInterface,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
+    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
   ) {}
+
+  private async _initDb() {
+    const mongoUri = getMongoURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    return this.databaseClient.connect(mongoUri);
+  }
 
   public async init() {
     this.logger.info('Application initialization');
@@ -16,5 +31,9 @@ export class RestApplication {
     this.logger.error('ehm', new Error('Some error'));
     this.logger.debug('This is debug');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
+    this.logger.info('Init database…');
+    await this._initDb();
+    this.logger.info('Init database completed');
   }
 }
