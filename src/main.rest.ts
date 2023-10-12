314cce5bece743c8b68f1c43c163cba1e72814b9
Greg <grtop@yandex.ru>
2023-10-12 16:00:19 +0300

    исправление ошибки

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,18 +1,20 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import { RestApplication } from './rest/index.js';
-import { Component } from './shared/types/index.js';
+import { AppComponent } from './shared/types/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
 import { createUserContainer } from './shared/libs/modules/user/index.js';
+import { createOfferContainer } from './shared/libs/modules/offer/index.js';
 
 
 async function bootstrap() {
   const appContainer = Container.merge(
     createRestApplicationContainer(),
     createUserContainer(),
+    createOfferContainer(),
   );
 
-  const application = appContainer.get<RestApplication>(Component.RestApplication);
+  const application = appContainer.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
 }
 
