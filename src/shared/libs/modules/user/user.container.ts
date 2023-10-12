b37695348e22ac37288e433253eec02a9399ff9d
Greg <grtop@yandex.ru>
2023-10-12 17:27:49 +0300

    правки

--- src/shared/libs/modules/user/user.container.ts
+++ src/shared/libs/modules/user/user.container.ts
@@ -1,14 +1,14 @@
 import { Container } from 'inversify';
 import { types } from '@typegoose/typegoose';
-import { UserService } from './user-service.interface.js';
-import { Component } from '../../../types/index.js';
-import { DefaultUserService } from './default-user.service.js';
+import { UserServiceInterface } from './user-service.interface.js';
+import { AppComponent } from '../../../types/index.js';
+import { UserService } from './default-user.service.js';
 import { UserEntity, UserModel } from './user.entity.js';
 
 export function createUserContainer() {
   const userContainer = new Container();
-  userContainer.bind<UserService>(Component.UserService).to(DefaultUserService).inSingletonScope();
-  userContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
+  userContainer.bind<UserServiceInterface>(AppComponent.UserServiceInterface).to(UserService).inSingletonScope();
+  userContainer.bind<types.ModelType<UserEntity>>(AppComponent.UserModel).toConstantValue(UserModel);
 
   return userContainer;
 }
