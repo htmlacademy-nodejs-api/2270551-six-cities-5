665950f3f4e32c82bd52ab4fba60ad4c8411b36a
Greg <grtop@yandex.ru>
2023-10-12 12:52:41 +0300

    перенос dto в module

--- src/shared/libs/modules/user/default-user.service.ts
+++ src/shared/libs/modules/user/default-user.service.ts
@@ -1,7 +1,7 @@
 import { UserService } from './user-service.interface.js';
 import { DocumentType, types } from '@typegoose/typegoose';
 import { UserEntity } from './user.entity.js';
-import { CreateUserDto } from './dto/create-user.dto.js';
+import { CreateUserDto } from '../dto/create-user.dto.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../../../types/index.js';
 import { LoggerInterface } from '../../../libs/logger/index.js';
