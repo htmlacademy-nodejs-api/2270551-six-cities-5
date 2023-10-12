3d73b8feecf39a8addbbdb9dd79890b2e9de3818
Greg <grtop@yandex.ru>
2023-10-12 17:32:42 +0300

    исправил UserService

--- src/shared/libs/modules/user/user.container.ts
+++ src/shared/libs/modules/user/user.container.ts
@@ -2,7 +2,7 @@ import { Container } from 'inversify';
 import { types } from '@typegoose/typegoose';
 import { UserServiceInterface } from './user-service.interface.js';
 import { AppComponent } from '../../../types/index.js';
-import { UserService } from './default-user.service.js';
+import UserService from './default-user.service.js';
 import { UserEntity, UserModel } from './user.entity.js';
 
 export function createUserContainer() {
