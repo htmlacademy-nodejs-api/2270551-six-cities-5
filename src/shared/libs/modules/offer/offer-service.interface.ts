083b00e408549c271f34c5f6e2f98712651cf30b
Greg <grtop@yandex.ru>
2023-10-14 12:01:39 +0300

    Расширит интерфейс default-offer.service

--- src/shared/libs/modules/offer/offer-service.interface.ts
+++ src/shared/libs/modules/offer/offer-service.interface.ts
@@ -1,8 +1,17 @@
+import { CityName } from '../../../types/city.type.js';
 import { OfferEntity } from './offer.entity.js';
 import { DocumentType } from '@typegoose/typegoose';
 import CreateOfferDto from '../dto/create-offer.dto.js';
+import UpdateOfferDto from '../dto/update-offer.dto.js';
 
 export interface OfferServiceInterface {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(id: string): Promise<DocumentType<OfferEntity> | null>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findPremiumByCity(cityName: CityName): Promise<DocumentType<OfferEntity>[]>
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  exists(documentId: string): Promise<boolean>;
 }
+
