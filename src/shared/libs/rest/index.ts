b6fbc67f7d2d6a23c872aabf7cade4bd1c4628f2
Greg <grtop@yandex.ru>
2023-11-02 00:53:30 +0300

    правка данных

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -7,4 +7,5 @@ export * from './request.params.type.js';
 export * from './middleware/middleware.interface.js';
 export * from '../../types/document-exists.interface.js';
 export * from './middleware/validate-objectid.middleware.js';
+export * from './middleware/upload-file.middleware.js';
 
